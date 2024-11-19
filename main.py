eab73f335ef4cc85dfba40112e2c80db7073cae0
Samurai <dan931901@gmail.com>
2024-11-19 20:55:08 +0300

    костяк внешнего вида

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
 import tkinter as tk
+from tkinter import ttk
+
 
 # Создание окна
 root = tk.Tk()
 root.title("Конвертер единиц")
-root.geometry("500x600")
+root.geometry("300x400")
+
+# Создание надписи над полем
+tk.Label(root, text="Введите значение:").pack()
+
+# Создание поля для значения
+entry_value = tk.Entry(root)
+entry_value.pack()
+
+# Создание двух выпадающих списков
+tk.Label(root, text="Из:").pack(pady=(20, 0))
+combo_from = ttk.Combobox(root)
+combo_from.pack()
+
+tk.Label(root, text="В:").pack(pady=(20, 0))
+combo_in = ttk.Combobox(root)
+combo_in.pack()
 
 # Создание кнопки
 button = tk.Button(root, text="Конвертировать", font=30)
-button.pack(side="bottom", pady=60)
+button.pack(pady=(20, 0))
+
+# Создание финальной строки
+result_label = tk.Label(root, text="Результат:")
+result_label.pack(pady=(20, 0))
 
 # Запуск приложения
 root.mainloop()
