1cd7705ccb133315633b74ccab9ed103e61c46f6
Samurai <dan931901@gmail.com>
2024-11-19 20:01:19 +0300

    создание кнопки

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ root = tk.Tk()
 root.title("Конвертер единиц")
 root.geometry("500x600")
 
-
+# Создание кнопки
+button = tk.Button(root, text="Конвертировать", font=30)
+button.pack(side="bottom", pady=60)
 
 # Запуск приложения
 root.mainloop()
